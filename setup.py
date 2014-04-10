82cb0f1864f51fee51fe5b666a2939ed86eb794f
Moritz Gellner <moritz.gellner@gmail.com>
2014-04-10 17:38:59 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(name='socialscraper',
+      version='0.0.1',
+      description='Scrapers for Social Networks',
+      author='Moritz Gellner',
+      author_email='moritz.gellner@gmail.com',
+      url='http://dev.alpaca.io/',
+      packages=['socialscraper',''],
+      install_requires=[
+      	'beautifulsoup4==4.3.2',
+      	'mechanize==0.2.5',
+      	'requests==2.2.1'
+      ]
+     )
