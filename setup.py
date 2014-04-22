c21404ee5b4999361e32830e21db98a6772fad15
Al Johri <al.johri@gmail.com>
2014-04-22 02:18:34 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -9,9 +9,13 @@ setup(name='socialscraper',
       packages=['socialscraper'],
       install_requires=[
       	'beautifulsoup4==4.3.2',
+	'enum34==0.9.23',
       	'mechanize==0.2.5',
+	'python-dateutil==2.2',
       	'requests==2.2.1',
-            'lxml==3.3.4',
-            'cssselect==0.9.1'
+        'lxml==3.3.4',
+        'cssselect==0.9.1',
+	'six==1.6.1',
+	'wsgiref==0.1.2'
       ]
      )
