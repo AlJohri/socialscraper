c663400111b82380d52158e43a41b860b0f23663
Moritz Gellner <moritz.gellner@gmail.com>
2014-04-26 17:13:48 -0500

    likes scraper w/o graph search

--- socialscraper/facebook/scraper.py
+++ socialscraper/facebook/scraper.py
@@ -1,6 +1,8 @@
 from ..base import BaseScraper, BaseUser, UsageError, ScrapingError
-import requests, pickle
+import requests, pickle, re
 from requests.adapters import HTTPAdapter
+
+from bs4 import BeautifulSoup
 
 from . import auth
 from . import graph
@@ -29,6 +31,11 @@ class FacebookScraper(BaseScraper):
         self.cur_user.username = auth.login(self.browser, self.cur_user.email, self.cur_user.password)
         self.cur_user.id = self.get_graph_id(self.cur_user.username)
 
+    def logout(self):
+        requests.post('http://www.facebook.com/logout.php')
+        print "Logged out."
+        return
+
     def get_graph_id(self, graph_name):
         return graph.get_id(graph_name)
 
@@ -39,12 +46,41 @@ class FacebookScraper(BaseScraper):
         return about.get(self.browser, self.cur_user, graph_name, graph_id=graph_id)
 
     def graph_search(self, graph_name, method_name, graph_id=None):
+        print "graph name: %s" % graph_name
+        print "method_name: %s" % method_name
         """Graph Search Wrapper."""
         for result in graph.search(self.browser, self.cur_user, graph_name, method_name, graph_id=graph_id): yield result
 
-    def get_pages_liked_by(self, user_name):
+    def _get_pages_liked_nograph(self, username):
+        url = "https://www.facebook.com/%s/likes" % username
+        resp = requests.get(url)
+        html = re.sub(r'(<!--)|(-->)',' ',resp.text)
+        soup = BeautifulSoup(html)
+
+        container = soup.findAll("div",["timelineFavorites"])[0]
+        
+        for link in container.findAll('a','mediaRowItem'):
+            try:
+                link['class']
+                yield { 'link': link['href'],
+                        'name': link.text }
+            except KeyError:
+                pass
+
+        for link in container.findAll('a'):
+            try:
+                link['class']
+            except KeyError:
+                yield { 'link': link['href'],
+                        'name': link.text }
+
+    def get_pages_liked_by(self, user_name, use_graph_search = False):
         """Graph Search Alias - pages-liked."""
-        for result in self.graph_search(user_name,"pages-liked"): yield result
+        if use_graph_search:
+            for result in self.graph_search(user_name,"pages-liked"): yield result
+        else:
+            for result in self._get_pages_liked_nograph(user_name):
+                yield result
 
     def get_likers_of_page(self, page_name):
         """Graph Search Alias - likers."""
