ea76252498c9fbe941ae75174456033891a97879
Al Johri <al.johri@gmail.com>
2014-05-02 19:09:06 -0500

    Fixed likes.

--- socialscraper/facebook/graphapi/likes.py
+++ socialscraper/facebook/graphapi/likes.py
@@ -1,7 +1,25 @@
+from time import time
+from datetime import datetime
 from ...base import ScrapingError
 
+from . import get_object
 from . import get_connections
 
 def get_likes(api, username):
     profile = get_connections(api, username, 'likes')
     return profile
+
+
+def get_likes(api, username):
+    after = ''
+    while True:
+        profile = api.get_object(username + "/likes", after=after)
+
+        if profile['data'] == []:
+            print "End of Results"
+            break
+        after = profile['paging']['cursors']['after']
+
+        for item in profile['data']:
+        	print item.get('name'), item.get('category'), item.get('id')
+            # print item.get('type') + ": " + item.get('story', '') + item.get('message', '')
