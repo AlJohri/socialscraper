88ea05b5235d8d0382b8ac8d8b58c22d100770cb
Michael Wang <michaelwang0712@gmail.com>
2014-05-16 17:18:24 -0500

    Added time to get_feed

--- socialscraper/facebook/graphapi/feed.py
+++ socialscraper/facebook/graphapi/feed.py
@@ -8,7 +8,28 @@ from . import get_object
 
 logger = logging.getLogger(__name__)
 
-def get_feed(api, username):
+def get_feed(api, username, earlier_date=None, later_date=None):
+    """
+
+    get_feed grabs posts reverse chronologically. it "starts" at the date 
+    specified in "until" and goes back in time until "since".
+
+    if until is not specified, until is set to the current date
+
+    if since is not specified, it grabs posts indefinitely in the past
+
+    """
+    
+    if not later_date:
+        until = int(time())
+    else:
+        until = later_date.strftime("%s")
+
+    if not earlier_date:
+        since = ""
+    else:
+        since = earlier_date.strftime("%s")
+
 
     def get_previous(previous_url):
         previous_url_parameters = parse_qs(urlparse(previous_url).query)
@@ -20,15 +41,14 @@ def get_feed(api, username):
 
     profile = get_object(api, username)
 
-    until = int(time())
     while True:
 
         # print "Getting Feed Until: " + datetime.utcfromtimestamp(until).strftime('%Y-%m-%d %H:%M:%S')
-        profile = api.get_object(username + "/feed", until=str(until))
+        profile = api.get_object(username + "/feed", until=str(until), since=str(since))
         if profile['data'] == []:
             # print "End of Results"
             break
-        since = get_previous(profile['paging']['previous'])
+        # since = get_previous(profile['paging']['previous'])
         until = get_next(profile['paging']['next'])
 
         for item in profile['data']:
