206f6bb711a357ed1ffcdc68afb8a3eba386462c
Andy Bayer <andrewbayer2016@u.northwestern.edu>
2014-11-22 18:03:21 -0600

    group scraper

--- demo/groups.py
+++ demo/groups.py
@@ -1,8 +1,12 @@
 import os, sys; sys.path.append(os.path.abspath('../'))
+import json
 
 from facebook import GraphAPI, GraphAPIError
 from socialscraper.facebook import graphapi
 FACEBOOK_USER_TOKEN = os.getenv('FACEBOOK_USER_TOKEN')
 api = GraphAPI(FACEBOOK_USER_TOKEN)
 
-print graphapi.get_object(api, "al.johri")
+
+for group in graphapi.get_object(api, "357518484295082/groups")['data']:
+	members = graphapi.get_object(api, group['id']+'/members')
+	name = group['name']
